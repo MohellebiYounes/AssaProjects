f7eae58051b71c1775a3663ccf36e93a45aae9f3
MohellebiYounes <98049578+MohellebiYounes@users.noreply.github.com>
2024-05-03 14:27:31 +0100

    filtre users

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,20 +1,23 @@
-import { LightningElement, track , api } from 'lwc';
+import { LightningElement } from 'lwc';
 
-export default class CreateUserWizard extends LightningElement {
-    @track selectedType = '';
 
-    // Define picklist options
+export default class helloWorld extends LightningElement {
+    selectedType = ''; // Ne pas définir comme @api
+
+    // Définissez les options de la liste de sélection
     get options() {
         return [
-            { label: 'Utilisateur BO', value: 'BO' },
+            { label: 'Utilisateur BO', value: 'Utilisateur BO' },
             { label: 'Animateur', value: 'Animateur' },
             { label: 'Livreur', value: 'Livreur' }
         ];
     }
-    // Handle type change event
+
+    // Gérez l'événement de changement de type
     handleTypeChange(event) {
-        this.selectedType = event.detail.value;
+        this.selectedType = event.target.value;
         const selectEvent = new CustomEvent('typechange', { detail: this.selectedType });
         this.dispatchEvent(selectEvent);
+        console.log('UserType:' + this.selectedType);  
     }
 }
